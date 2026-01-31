3cacb1f4f08ff675922653139f16b33d174d75ec
IceRain <130889058+96syh@users.noreply.github.com>
2026-01-31 22:21:03 +0800

    Upload l/tools.h

---
+++ l/tools.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <graphics.h>
+
+int getDelay(); 
+
+void putimagePNG(int  picture_x, int picture_y, IMAGE* picture);
+
+void putimagePNG2(int x, int y, IMAGE* picture);
+void putimagePNG2(int x, int y, int winWidth, IMAGE* picture);
+
+bool rectIntersect(int a1X, int a1Y, int a2X, int a2Y,
+    int b1X, int b1Y, int b2X, int b2Y);
+
+void  preLoadSound(const char* name);
+void  playSound(const char* name);
+
+void drawBloodBar(int x, int y, int width, int height, int lineWidth, int boardColor, int emptyColor, int fillColor, float percent);
